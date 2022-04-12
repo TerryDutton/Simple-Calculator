0f20a35806f3f1e3acb5f585e04c531cf71a3bd3
Terry Dutton <terry.dutton@thinkmoney.co.uk>
2022-04-12 16:32:19 +0100

    Rewrote functionality. Calculator user can type digits.

--- script.js
+++ script.js
@@ -1,68 +1,123 @@
-const display       = document.getElementById('digit-display');
-const baseDigit     = document.getElementById('digit-template').content.firstElementChild;
-const basePoint     = document.getElementById('decimal-point-template').content.firstElementChild; 
-const decimalBtn    = document.getElementById('decimal-btn');
-const clearEntryBtn = document.getElementById('clear-entry');
-const clearAllBtn   = document.getElementById('clear');
-const numberBtns    = document.querySelectorAll('button.number');      
+const digitDisplay    = document.getElementById('digit-display');
+const operatorDisplay = document.getElementById('operator-display');
+const baseDigit       = document.getElementById('digit-template').content.firstElementChild;
+const basePoint       = document.getElementById('decimal-point-template').content.firstElementChild; 
+const decimalPointBtn = document.getElementById('decimal-btn');
+const equalsBtn       = document.getElementById('equals-btn');
+const clearEntryBtn   = document.getElementById('clear-entry');
+const clearAllBtn     = document.getElementById('clear');
+const numberBtns      = document.querySelectorAll('button.number');
+const operatorBtns    = document.querySelectorAll('button.operator');
 
 const displayLength = 10;
-const digits = Array.from({length: displayLength}, _ => baseDigit.cloneNode(true));
-const points = Array.from({length: displayLength}, _ => basePoint.cloneNode(true));
+const digits = Array.from({length: displayLength}, () => baseDigit.cloneNode(true));
+const points = Array.from({length: displayLength}, () => basePoint.cloneNode(true));
+const calcFunctions = {
+    "+": (a,b) => a + b,
+    "-": (a,b) => a - b,
+    "x": (a,b) => a * b,
+    "÷": (a,b) => a / b
+};
+
+let isResetDisplayOnNextInput = false;
+let lastInputWasOperator = false;
+let firstOperand = null;
+let secondOperand = null;
+let displayedValue = "0";
+updateDisplay();
 
 digits.forEach((digit, i) => {
-    display.appendChild(digit);
-    display.appendChild(points[i]);
+    digitDisplay.appendChild(digit);
+    digitDisplay.appendChild(points[i]);
+});
+numberBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        addCharacterToDisplay(e.target.textContent);
+    });
+});
+operatorBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        setOperator(e.target.textContent);
+    });
 });
 
-function setCharacters(value){
+clearEntryBtn.addEventListener('click', deleteCharacter);
+decimalPointBtn.addEventListener('click', addDecimalPointToDisplay);
+
+function setDisplayedCharacters(value){
     const valueToSet = value.replace(/\./, "");
     const blankSpace = ' '.repeat(displayLength - valueToSet.length);
     const charList = [...`${blankSpace}${valueToSet}`];
     charList.forEach((char, i) => digits[i].setAttribute('data-character', char));
 }
 
-function setDecimalPoint(value){
+function setDisplayedDecimalPoints(value){
+    const attr = 'data-active';
     let activePointIndex = value.indexOf('.');
     if (activePointIndex > -1) activePointIndex += (displayLength - value.length);
-    
-    const attr = 'data-active';
     points.forEach((point, i) => {
         if (i === activePointIndex) point.setAttribute(attr,`${attr}`);
         else point.removeAttribute(attr);
     });
 }
 
-let displayedCharacters = "";
-addCharacterToDisplay("0");
+function updateDisplay(){
+    setDisplayedCharacters(displayedValue);
+    setDisplayedDecimalPoints(displayedValue);
+}
+
+function calculate(a, b, operator){
+    if (calcFunctions[operator]) return calcFunctions[operator](a,b);
+    else throw new TypeError(`Invalid operator "${operator}".`);
+}
+
+function setOperator(operator){
+    operatorDisplay.textContent = operator;
+    isResetDisplayOnNextInput = true;
+}
 
 function addCharacterToDisplay(char){
-    if(displayedCharacters === "0" && char !== ".") displayedCharacters = char;
-    else displayedCharacters += char;
+    if(!isResetDisplayOnNextInput && displayHasMaxCharacters()) return;
+    if (isResetDisplayOnNextInput || displayedValue === "0") displayedValue = char;
+    else displayedValue += char;
+    isResetDisplayOnNextInput = false;
     updateDisplay();
 }
 
-function deleteLastCharacter(){
-    const slice = displayedCharacters.slice(0, -1);
-    if(slice === '') displayedCharacters = "0";
-    else displayedCharacters = slice;
+function displayHasMaxCharacters(){
+    return displayedValue.replace(/\D/g, "").length >= displayLength;
+}
+
+
+function addDecimalPointToDisplay(){
+    if(displayedValue.includes(".") && !isResetDisplayOnNextInput) return;
+    if(displayedValue === "" || isResetDisplayOnNextInput) displayedValue = "0.";
+    else displayedValue += ".";
+    isResetDisplayOnNextInput = false;
     updateDisplay();
 }
 
-function updateDisplay(){
-    setCharacters(displayedCharacters);
-    setDecimalPoint(displayedCharacters);
+function deleteCharacter(){
+    displayedValue = displayedValue.slice(0, -1);
+    if(!displayedValue) displayedValue = "0";
+    updateDisplay();
 }
 
-numberBtns.forEach(btn => {
-    btn.addEventListener('click', e => {
-        addCharacterToDisplay(e.target.textContent);
-    });
-});
+function clearAll(){
+    isResetDisplayOnNextInput = false;
+    lastInputWasOperator = false;
+    firstOperand = null;
+    secondOperand = null;
+    displayedValue = "0";
+    updateDisplay();
+}
 
-decimalBtn.addEventListener('click', () => {
-    if(displayedCharacters.includes(".")) return;
-    addCharacterToDisplay(".");
-});
+/* What are the ways I want this to behave?
+1. When you input a number, you input the digits one at a time and they appear in sequence.
+2. When you press an operator button for the first time, it sets an operator.
+3. When you enter a second number, it replaces the first one.
+4. When you press equals, it does the set operation on the first and second numbers.
+5. When you press equals again, it does the same operation on the displayed number with the same second operand. */
 
-clearEntryBtn.addEventListener('click', deleteLastCharacter);
+/* So what we're looking at here is... input to the first operand. Then after the first operator, 
+all subsequent inputs are saved to the second operand, and all subsequent  */
